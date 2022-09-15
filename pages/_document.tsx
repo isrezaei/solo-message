c3eef5104534cd48efa65d268c831269fa6e0559
isrezei <rezaeiism@yahoo.com>
2022-09-15 22:24:48 +0430

    reinstall tailwind v3 !

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
                     rel="stylesheet"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="https://user-images.githubusercontent.com/77073972/190473962-0d101210-910c-4bac-9d1b-79a0d9326f4b.png" />
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet"/>
