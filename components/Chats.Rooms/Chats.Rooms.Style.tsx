f6ee2a378b1a1667c8b7e4e5ff013a2e5ad0366b
isrezei <rezaeiism@yahoo.com>
2022-09-15 18:39:25 +0430

    reBuild app , reWrite code  server side is off , reWrite styles beta 0.1 try to fix style for mobile browser

--- components/Chats.Rooms/Chats.Rooms.Style.tsx
+++ components/Chats.Rooms/Chats.Rooms.Style.tsx
@@ -21,9 +21,9 @@ export const Body = styled.div`
 
   width: 100%;
   height: 37rem;
-  background: #2f2f2f;
-  padding: 0rem 1rem ;
+  padding: 0rem 1rem;
   overflow-y: scroll;
+
   ::-webkit-scrollbar {
     display: none;
   }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import type {AppProps} from 'next/app'
 import {GlobalStyle} from "../styles/Global.Style";
 import '../styles/_app.css'
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "../config/Firebase";
-import {Login} from "../components/Login/Login";
-import {ChatsCollection} from "../components/Chats.Collection/Chats.Collection";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-    const [user, loading , error] = useAuthState(auth);
-
-    // if (loading) return <h1>Loading...</h1>
-    // if (!user) return <Login/>
-
-
     return (
       <>
         <GlobalStyle/>
